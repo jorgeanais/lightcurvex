20aa6bac096b07c447fff5a83079b927acce652a
Jorge Anais <jrganais@gmail.com>
2022-10-04 16:28:06 -0300

    Include filter in the log file

--- scripts/convert_tables.sh
+++ scripts/convert_tables.sh
@@ -6,6 +6,7 @@ OUTPUT_FILE="catalogue.asc"
 OUTPUT_DIR="ascii_tables"
 LOG_FILE="obsdates.txt"
 GETDATEOBS() { fitsheader -k DATE-OBS $1 | grep -Eho "'.*'" | head -n 1; };
+GETFILTER() { fitsheader -k "HIERARCH ESO INS FILT1 NAME" $1 | grep -Eho "'.*'" | head -n 1; };
 
 rm -rf $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
@@ -16,7 +17,8 @@ do
 
     # Extract observation date from header and save it to a log file
     dateobs=$(GETDATEOBS $file)
-    echo "$file $dateobs" >> $OUTPUT_DIR/$LOG_FILE
+    filter=$(GETFILTER $file)
+    echo "${file%.fits} $dateobs $filter" >> $OUTPUT_DIR/$LOG_FILE
     
     # Process the .fits table
     fitsio_cat_list $file
