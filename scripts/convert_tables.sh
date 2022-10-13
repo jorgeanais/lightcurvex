d01a29587baf4a3e072645e017474191f0588bc1
Jorge Anais <jrganais@gmail.com>
2022-10-13 10:16:23 -0300

    Bash script now incorporates MJD extracted directly from the header, and also raw files are now separated in a different folder

--- scripts/convert_tables.sh
+++ scripts/convert_tables.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
 # Convert fits tables to ascii file. Observation date is saved to a log file.
+# Reference regex lookahead: https://stackoverflow.com/questions/1454913/regular-expression-to-find-a-string-included-between-two-characters-while-exclud
 
 OUTPUT_FILE="catalogue.asc"
+INPUT_DIR="raw_fits_tables"
 OUTPUT_DIR="ascii_tables_no-tiled"
 LOG_FILE="log.txt"
 GETDATEOBS() { fitsheader -k DATE-OBS $1 | grep -Eho "'.*'" | head -n 1; };
+GETMJDOBS() { fitsheader -k MJD-OBS $1 | grep -Pho "(?<==).*(?=\s/)" | head -n 1; };
 GETFILTER() { fitsheader -k "HIERARCH ESO INS FILT1 NAME" $1 | grep -Eho "'.*'" | head -n 1; };
+
 
 rm -rf $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
 
-for file in *_st_cat.fits;
+for file in $INPUT_DIR/*_st_cat.fits;
 do
     echo "Processing $file"
 
     # Extract observation date from header and save it to a log file
     dateobs=$(GETDATEOBS $file)
     filter=$(GETFILTER $file)
-    echo "${file%.fits} $dateobs $filter" >> $OUTPUT_DIR/$LOG_FILE
+    mjd=$(GETMJDOBS $file)
+    filestem=$(basename $file .fits)
+    echo "$filestem $dateobs $filter '$mjd'" >> $OUTPUT_DIR/$LOG_FILE
     
     # Process the .fits table
     fitsio_cat_list $file
-    mv -- $OUTPUT_FILE $OUTPUT_DIR/${file%.fits}.asc
+    mv -- $OUTPUT_FILE $OUTPUT_DIR/$filestem.asc
 done;
