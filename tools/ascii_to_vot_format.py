17e507a3f660293ec2e24abad8731541f6569d51
Jorge Anais <jrganais@gmail.com>
2022-10-14 10:35:07 -0300

    Added some python tools to aggregate output data

---
+++ tools/ascii_to_vot_format.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+"""
+Save ascii catalogs to votable format after coords processing
+"""
+import sys
+sys.path.append('../')
+
+from pathlib import Path
+
+from src.data.loader import load_catalogs
+
+
+DATA_DIR = Path("/home/jorge/Documents/data/CASU_411/tables/ascii_tables_no-tiled/")
+OUTPUT_DIR = Path("/home/jorge/Documents/data/CASU_411/tables/ascii_tables_no-tiled/votable/")
+
+# Time consuming part
+catalogs = load_catalogs(DATA_DIR)
+
+# Save catalogs in VOT format
+if not OUTPUT_DIR.exists():
+    OUTPUT_DIR.mkdir(parents=True)
+
+for catalog in catalogs:
+    catalog.table.write(str(OUTPUT_DIR / f"{catalog.name}.vot"), format="votable")
+    
---
+++ tools/join_output_dephase_objects.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python
+"""
+Condense all the csv files into a single table with one row per object
+"""
+import pandas as pd
+from pathlib import Path
+
+
+INPUT_DIR = Path("/home/jorge/Documents/data/CASU_411/tables/ascii_tables_no-tiled/output_dephase/")
+
+fixed_columns = ["GaiaDR2", "Gmag", "RA_ICRS", "DE_ICRS", "pmRA", "pmDE",  "SOStype", "VCtype", "PS1type", "period"]
+cols_to_group = ["filter", "mag"]
+
+rows = []
+
+for file in INPUT_DIR.glob("*.csv"):
+    print(file)
+    df = pd.read_csv(file)
+    sfix = df[fixed_columns].iloc[0]
+    smean = df[cols_to_group].groupby("filter").mean().T.iloc[0]
+
+    row = pd.concat([sfix,smean], axis=0)
+    rows.append(row)
+
+
+df = pd.DataFrame(rows)
+print(df)
+df.to_csv("agg_values_RRLyrae.csv", index=False)
