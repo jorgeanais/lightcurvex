76721dc3451d5d06b4fef5b00264b059911fe1ea
Jorge Anais <jrganais@gmail.com>
2022-10-17 11:17:30 -0300

    Added query to use Soszynski 2017 RRLyrae catalog with better period

--- tools/useful_queries.sql
+++ tools/useful_queries.sql
@@ -10,3 +10,8 @@ FROM gaiadr2.vari_rrlyrae RR
     LEFT JOIN gaiadr2.gaia_source as GS 
         ON RR.source_id = GS.source_id
 WHERE GS.l >= 5.0128 AND GS.l <= 6.5309 AND GS.b >= -14.5977 AND GS.b <= -13.3988 
+
+-- Get all OGLE RR Lyrae from Soszynski+ 2017
+SELECT * 
+FROM "J/AcA/67/297/rrlyr" RR
+    LEFT JOIN "J/AcA/67/297/ident" ID ON RR.recno = ID.recno
